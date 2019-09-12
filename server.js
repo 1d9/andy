657a91c05120f0999de0ad2e198d1f8578987bdb
Unknown <luke@kaal.im>
2019-09-12 11:50:53 +1000

    write new line on sigint

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // @flow strict
 const { createAndy } = require('./src/andy');
+const { EOL } = require('os')
 
 const server = (port) => {
   const andy = createAndy(port);
   andy.start();
   process.on('SIGINT', () => {
+    process.stdout.write(EOL);
     andy.stop();
   })
 };
